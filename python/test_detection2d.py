02b6fc38a4e871570248a7b3c73669ce27d5e59e
chengguizi <cgz_05@sina.com>
2022-12-30 15:22:17 +0800

    update detection definition, put detection in the last

--- python/test_detection2d.py
+++ python/test_detection2d.py
@@ -23,12 +23,12 @@ def frameNorm(frame, bbox):
     normVals[::2] = frame.shape[1]
     return (np.clip(np.array(bbox), 0, 1) * normVals).astype(int)
 
-def displayFrame(frame, detections):
+def displayFrame(frame, detections, labels):
     color = (255, 0, 0)
     for detection in detections:
         bbox = frameNorm(frame, (detection.xmin, detection.ymin, detection.xmax, detection.ymax))
-        print(f"{detection.labelString} : {bbox}")
-        cv2.putText(frame, detection.labelString, (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
+        print(f"{labels[detection.labelIdx]} : {bbox}")
+        cv2.putText(frame, labels[detection.labelIdx], (bbox[0] + 10, bbox[1] + 20), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
         cv2.putText(frame, f"{int(detection.confidence * 100)}%", (bbox[0] + 10, bbox[1] + 40), cv2.FONT_HERSHEY_TRIPLEX, 0.5, 255)
         cv2.rectangle(frame, (bbox[0], bbox[1]), (bbox[2], bbox[3]), color, 2)
 
@@ -36,16 +36,32 @@ def callback(topic_name, msg, ts):
 
     with eCALDetection2d.Detections2d.from_bytes(msg) as d:
         print(f"seq = {d.header.seq}, with {len(msg)} bytes")
-        print(f"width = {d.width}, height = {d.height}")
+        print(f"width = {d.image.width}, height = {d.image.height}")
 
-        
-        print(len(d.imageData))
-        mat = np.frombuffer(d.imageData, dtype=np.uint8)
-        # mat = mat.reshape((640, 640, 1))
-        mat = mat.reshape((d.height, d.width, 3))
+        if (d.image.encoding == "mono8"):
+
+            mat = np.frombuffer(d.image.data, dtype=np.uint8)
+            mat = mat.reshape((d.image.height, d.image.width, 1))
+            mat = cv2.cvtColor(mat, cv2.COLOR_GRAY2BGR)
+
+        elif (d.image.encoding == "yuv420"):
+            mat = np.frombuffer(d.image.data, dtype=np.uint8)
+            mat = mat.reshape((d.image.height * 3 // 2, d.image.width, 1))
+            mat = cv2.cvtColor(mat, cv2.COLOR_YUV2BGR_IYUV)
+
+        elif (d.image.encoding == "bgr8"):
+            mat = np.frombuffer(d.image.data, dtype=np.uint8)
+            mat = mat.reshape((d.image.height, d.image.width, 3))
+
+        elif (d.image.encoding == "jpeg"):
+            mat_jpeg = np.frombuffer(d.image.data, dtype=np.uint8)
+            mat = cv2.imdecode(mat_jpeg, cv2.IMREAD_COLOR)
+
+        else:
+            raise RuntimeError("unknown encoding: " + d.image.encoding)
 
 
-        displayFrame(mat, d.detections)
+        displayFrame(mat, d.detections, d.labels)
         # bbox = np.zeros(4)
         # bbox[:] = [msg.xmin, msg.ymin, msg.xmax, msg.ymax]
 
@@ -63,8 +79,18 @@ def main():
     # set process state
     ecal_core.set_process_state(1, 1, "I feel good")
 
+    n = len(sys.argv)
+    if n == 1:
+        topic = "S0/yolo/camb"
+    elif n == 2:
+        topic = sys.argv[1]
+    else:
+        raise RuntimeError("Need to pass in exactly one parameter for topic")
+
+    print(f"Streaming topic {topic}")
+
     # create subscriber and connect callback
-    sub = ByteSubscriber("S0/yolo/cama")
+    sub = ByteSubscriber(topic)
     # sub = ByteSubscriber("raw_fisheye_image")
     # sub = ByteSubscriber("S0/stereo1_l")
     sub.set_callback(callback)
--- src/capnp/detection2d.capnp
+++ src/capnp/detection2d.capnp
@@ -6,12 +6,11 @@ $Cxx.namespace("ecal");
 struct Detection2d {
 
     labelIdx @0 :UInt32;
-    labelString @1 :Text;
-    xmin @2 :Float32;
-    xmax @3 :Float32;
-    ymin @4 :Float32;
-    ymax @5 :Float32;
-    confidence @6 :Float32;
+    xmin @1 :Float32;
+    xmax @2 :Float32;
+    ymin @3 :Float32;
+    ymax @4 :Float32;
+    confidence @5 :Float32;
 
 }
 
@@ -19,10 +18,11 @@ struct Detections2d {
 
     header @0 :import "header.capnp".Header;
 
-    detections @1 :List(Detection2d);
+    labels @1 :List(Text);
 
-    width @2 :UInt32;
-    height @3 :UInt32;
+    image @2 :import "image.capnp".Image;
 
-    imageData @4 :Data; # should be RGB
+    detections @3 :List(Detection2d);
+
+    
 }
