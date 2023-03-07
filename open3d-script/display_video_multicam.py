fb3a5f4fe58e986bc9713db8221f9757ff87b37b
Tan Haoxuan <tanhaoxuan0519@gmail.com>
2023-03-07 17:34:48 +0800

    able to stream 3 cameras and display real time information in GUI

--- open3d-script/display_video_multicam.py
+++ open3d-script/display_video_multicam.py
@@ -10,7 +10,6 @@ import cv2
 import platform
 
 import ecal.core.core as ecal_core
-# from byte_subscriber import ByteSubscriber
 
 capnp.add_import_hook(['../src/capnp'])
 
@@ -21,7 +20,7 @@ import open3d.visualization.gui as gui
 
 from PIL import Image
 
-from utils import SyncedImageSubscriber, image_resize
+from utils import SyncedImageSubscriber
 
 isMacOS = (platform.system() == "Darwin")
 
@@ -40,64 +39,78 @@ class VideoWindow:
 
         self.streaming_status = False
 
-        # configure window
+        # CONFIGURE WINDOW
         self.window = gui.Application.instance.create_window(
-            "Open3D - Video", 1400, 850)
+            "Open3D - Video", 1100, 850)
         self.window.set_on_close(self._on_close)
 
-        # configure menu
+        # CONFIGURE MENU
         if gui.Application.instance.menubar is None:
             if isMacOS:
                 app_menu = gui.Menu()
                 app_menu.add_item("Quit", VideoWindow.MENU_QUIT)
+            
+            # first sub menu
             debug_menu = gui.Menu()
             if not isMacOS:
-                debug_menu.add_separator()
+                # debug_menu.add_separator()
                 debug_menu.add_item("Quit", VideoWindow.MENU_QUIT)
-
-            menu = gui.Menu()
+            
+            # all the submenu collection
+            menu = gui.Menu() 
             if isMacOS:
                 # macOS will name the first menu item for the running application
                 # (in our case, probably "Python"), regardless of what we call
                 # it. This is the application menu, and it is where the
                 # About..., Preferences..., and Quit menu items typically go.
                 menu.add_menu("Example", app_menu)
-                menu.add_menu("Debug", debug_menu)
+                menu.add_menu("Menu", debug_menu)
             else:
-                menu.add_menu("Debug", debug_menu)
+                menu.add_menu("Menu", debug_menu)
             gui.Application.instance.menubar = menu
 
-        # The menubar is global, but we need to connect the menu items to the
-        # window, so that the window can call the appropriate function when the
-        # menu item is activated.
-
+        # connect the menu items to the window
         self.window.set_on_menu_item_activated(VideoWindow.MENU_QUIT,
                                                self._on_menu_quit)
 
 
 
-        # configure gui
+        # CONFIGURE GUI
         em = self.window.theme.font_size
         margin = 0.5 * em
+        
+        # main panel
         self.panel_main = gui.Vert(0.5 * em, gui.Margins(margin))
 
+        # top panel
         self.panel_top = gui.Horiz(0.5 * em, gui.Margins(margin))
-
         self.rgb_widget_1 = gui.ImageWidget(o3d.geometry.Image(np.zeros((400,640,1), dtype=np.uint8)))
         self.rgb_widget_2 = gui.ImageWidget(o3d.geometry.Image(np.zeros((400,640,1), dtype=np.uint8)))
         self.panel_top.add_child(self.rgb_widget_1)
         self.panel_top.add_child(self.rgb_widget_2)
-        
         self.panel_main.add_child(self.panel_top) 
 
+        # bottom panel
         self.panel_bottom = gui.Horiz(0.5 * em, gui.Margins(margin))
 
+        # side 
         collapse = gui.CollapsableVert("Widgets", 0.33 * em,
                                        gui.Margins(em, 0, 0, 0))
-        self._label = gui.Label("control")
-        self._label.text_color = gui.Color(1.0, 0.5, 0.0)
+        
+        self.proxy_1 = gui.WidgetProxy()
+        collapse.add_child(self.proxy_1)
+        
+        self.proxy_2 = gui.WidgetProxy()
+        collapse.add_child(self.proxy_2)
 
-        collapse.add_child(self._label)
+        self.proxy_3 = gui.WidgetProxy()
+        collapse.add_child(self.proxy_3)
+
+        self.label = gui.Label("control")
+        self.label.text_color = gui.Color(1.0, 0.5, 0.0)
+
+        collapse.add_child(self.label)
+        
         cb = gui.Checkbox("Start streaming")
         cb.set_on_checked(self._on_cb)  # set the callback function
         collapse.add_child(cb)
@@ -159,11 +172,21 @@ def read_img(window):
         if(imageName == 'S0/camd'):
             window.rgb_widget_3.update_image(o3d.geometry.Image(img_ndarray))
 
-    while ecal_core.ok():
+    def update_proxy(proxy,display_msg):
         
+        label = gui.Label(display_msg + '\n')
+        proxy.set_widget(label)
+
+        window.window.set_needs_layout()
+
+
+
+
+    while ecal_core.ok():
+
         # READ IN DATA
         ecal_images = recorder.image_sub.pop_sync_queue()
-        timestamp1 = time.monotonic()
+        
         for imageName in ecal_images:
 
             imageMsg = ecal_images[imageName]
@@ -171,16 +194,17 @@ def read_img(window):
             img_ndarray = np.frombuffer(imageMsg.data, dtype=np.uint8)
             img_ndarray = img_ndarray.reshape((imageMsg.height, imageMsg.width, 1))
             
-            expTime_display = f"expTIme = {imageMsg.exposureUSec}"
+            expTime_display = f"expTime = {imageMsg.exposureUSec}"
             sensIso_display = f"sensIso = {imageMsg.gain}"
             latencyDevice_display = f"latency device = {imageMsg.header.latencyDevice / 1e6} ms"
             latencyHost_display = f"latency host = {imageMsg.header.latencyHost / 1e6} ms"
+
+            all_display = imageName + '\n' + expTime_display + '\n' + sensIso_display + '\n' + latencyDevice_display + '\n' + latencyHost_display 
 
             img_ndarray = cv2.putText(img_ndarray, expTime_display, (100,100), cv2.FONT_HERSHEY_TRIPLEX, 1, (255,0,0), 2)
             img_ndarray = cv2.putText(img_ndarray, sensIso_display, (100,140), cv2.FONT_HERSHEY_TRIPLEX, 1, (255,0,0), 2)
             img_ndarray = cv2.putText(img_ndarray, latencyDevice_display, (100,180), cv2.FONT_HERSHEY_TRIPLEX, 1, (255,0,0), 2)    
             img_ndarray = cv2.putText(img_ndarray, latencyHost_display, (100,220), cv2.FONT_HERSHEY_TRIPLEX, 1, (255,0,0), 2)    
-    
             
             # resize to smaller resolution
             scale_percent = 40 # percent of original size
@@ -196,16 +220,21 @@ def read_img(window):
 
 
             if not window.is_done and window.streaming_status:
+
                 update_frame(imageName, img_ndarray)
+
+                if(imageName == 'S0/camb'):
+                    update_proxy(window.proxy_1,all_display)
+                if(imageName == 'S0/camc'):
+                    update_proxy(window.proxy_2,all_display)
+                if(imageName == 'S0/camd'):
+                    update_proxy(window.proxy_3,all_display)
         
         window.window.post_redraw()
 
 
-
     # finalize eCAL API
     ecal_core.finalize()
-
-
 
 
 
@@ -225,7 +254,6 @@ def main():
     app.run()
     
         
-
 
 if __name__ == "__main__":
     
