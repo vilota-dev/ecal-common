0a080a45c753ad5589e7c19e18e702ebb972ca5e
Gokul Rajiv <gokul.rajiv99@gmail.com>
2023-07-10 15:00:41 +0800

    Add synchronizer callback mode

--- cpp/include/ecal_camera/CameraInterface.hpp
+++ cpp/include/ecal_camera/CameraInterface.hpp
@@ -15,10 +15,17 @@
 namespace vk 
 {
 
-template <typename T>
+enum SynchroniserReturnMode {
+    CALLBACK_MODE,
+    POLLING_MODE
+};
+
+template <typename T, SynchroniserReturnMode M = POLLING_MODE>
 class MessageSynchroniserExact {
 
   public:
+    using CallbackF = std::function<void(std::vector<T>)>;
+
     void init(size_t N, const std::vector<std::string> &names = {},
               std::string prefix = {}, size_t queueSize = 10, bool realtime = true) {
         m_N = N;
@@ -100,11 +107,26 @@ class MessageSynchroniserExact {
             } else {
                 cleanupQueues();
             }
+
+            if (M == CALLBACK_MODE) {
+                auto ret = tryGet(true);
+                if (ret.size()) {
+                    std::for_each(m_callbacks.begin(), m_callbacks.end(), [&](auto& cb) {
+                        cb(ret);
+                    });
+                }
+            }
         }
     }
 
-    std::vector<T> tryGet() {
+    template <SynchroniserReturnMode U = M>
+    std::enable_if_t<U == POLLING_MODE, std::vector<T>> tryGet() {
         return tryGet(false);
+    }
+
+    template <SynchroniserReturnMode U = M>
+    std::enable_if_t<U == CALLBACK_MODE, void> registerCallback(CallbackF cb) {
+        m_callbacks.push_back(cb);
     }
 
   private:
@@ -194,6 +216,7 @@ class MessageSynchroniserExact {
 
     size_t m_queueSize;
     bool m_realtime;
+    std::vector<CallbackF> m_callbacks{};
 };
 
 struct CameraParams {
@@ -207,6 +230,8 @@ struct CameraParams {
 
     std::vector<std::string> idxTopicMap; // being populated, without prefix
     std::map<std::string, uint16_t> topicIdxMap; // being populated, without prefix
+
+    std::string product_name;
 };
 
 struct CameraCalibration {
